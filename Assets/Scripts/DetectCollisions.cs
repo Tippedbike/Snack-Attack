408df042fca85212b912c0ec73e55873f1d535e7
Tippedbike <113185655+Tippedbike@users.noreply.github.com>
2023-12-14 21:18:09 -0500

    it cannot translate

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
-    int scoreToAdd = 0;
+    int scoreToAdd;
     
 // Start is called before the first frame update
 void Start()
 { 
+
 }
-void OnTriggerEnter(Collider other)
+ void OnTriggerEnter(Collider other)
 {
 if(other.CompareTag("Player"))
 {
      PlayerController pc = other.GetComponent<PlayerController>();
-
+     Destroy(gameObject);
      pc.score += scoreToAdd;
-     Debug.Log(scoreToAdd);
+     Debug.Log(pc.score);
 }
 }
 // Update is called once per frame
